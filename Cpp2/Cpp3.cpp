9a1825150561b058aee0feca4f9a843f54010c1c
Joël Randriamalala <166411113+Joel68randria@users.noreply.github.com>
2025-02-14 15:30:48 +0300

    Update Cpp3.cpp

--- Cpp2/Cpp3.cpp
+++ Cpp2/Cpp3.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+    int main (){
+        int a;
+        std::cout<<"Entrez une valeur pour faire sa multiplication 	\n";
+        std::cin>>a;
+        for (int i = 0 ; i <= 10;i++ ){
+            std::cout<<a<<" × "<<i<<" = "<<a*i<<"\n";
+        }
+    }
